65760a6d39ee432db0c0b3673379b00769c8fea8
Daniele Oliveira <62821027+DanieleOliveira1@users.noreply.github.com>
2022-01-28 19:13:28 -0300

    Aula 5
    finalização básica do projeto.

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,7 @@ export default function PaginaInicial() {
             onSubmit={function (infosDoEvento) {
               infosDoEvento.preventDefault();
               console.log('Alguém submeteu o form');
-              roteamento.push('/chat');
+              roteamento.push(`/chat?username=${username}`);
               // window.location.href = '/chat';
             }}
 
